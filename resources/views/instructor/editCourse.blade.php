dfc9a3d1b93bbf635ba52ab8c0f6696ebb4aa14e
Thomas <thomasvsachs@gmail.com>
2020-06-14 12:46:40 +0200

    tabs im edit course screen

--- resources/views/instructor/editCourse.blade.php
+++ resources/views/instructor/editCourse.blade.php
@@ -11,24 +11,50 @@
 
     <h1>Edit {{ $course->title }}</h1>
 
-    {!! Form::open(['action' => ['CoursesController@update', $course->id], 'method' => 'POST']) !!}
-        <div class="form-group">
-            {{ Form::label('title', 'Title*') }}
-            {{ Form::text('title', $course->title, ['class' => 'form-control', 'placeholder' => 'Title']) }}
-        </div>
-        <div class="form-group">
-            {{ Form::label('language', 'Language*') }}
-            {{ Form::text('language', $course->language, ['class' => 'form-control', 'placeholder' => 'Language']) }}
-        </div>
-        <div class="form-group">
-            {{ Form::label('game', 'Name of the Game*') }}
-            {{ Form::text('game', $course->game, ['class' => 'form-control', 'placeholder' => 'Gamename']) }}
-        </div>
-        {{ Form::hidden('_method', 'PUT') }}
-        {{ Form::submit('Edit', ['class' => 'btn btn-primary']) }}
-    {!! Form::close() !!}
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+      <li class="nav-item">
+        <a class="nav-link active" id="general-tab" data-toggle="tab" href="#general" role="tab" aria-controls="general" aria-selected="true">General</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" id="description-tab" data-toggle="tab" href="#description" role="tab" aria-controls="description" aria-selected="false">Description</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" id="lectures-tab" data-toggle="tab" href="#lectures" role="tab" aria-controls="lectures" aria-selected="false">Lectures</a>
+      </li>
+    </ul>
 
-    laravel part 8 minute 3:50 für put spoof
+
+    <div class="tab-content" id="myTabContent">
+      <div class="tab-pane fade show active" id="general" role="tabpanel" aria-labelledby="general-tab">
+
+          {!! Form::open(['action' => ['CoursesController@update', $course->id], 'method' => 'POST']) !!}
+              <div class="form-group">
+                  {{ Form::label('title', 'Title') }}
+                  {{ Form::text('title', $course->title, ['class' => 'form-control', 'placeholder' => 'Title']) }}
+              </div>
+              <div class="form-group">
+                  {{ Form::label('language', 'Language') }}
+                  {{ Form::text('language', $course->language, ['class' => 'form-control', 'placeholder' => 'Language']) }}
+              </div>
+              <div class="form-group">
+                  {{ Form::label('game', 'Name of the Game') }}
+                  {{ Form::text('game', $course->game, ['class' => 'form-control', 'placeholder' => 'Gamename']) }}
+              </div>
+              {{ Form::hidden('_method', 'PUT') }}
+              {{ Form::submit('Edit', ['class' => 'btn btn-primary']) }}
+          {!! Form::close() !!}
+      </div>
+
+      <div class="tab-pane fade" id="description" role="tabpanel" aria-labelledby="description-tab">
+
+
+          description
+
+
+      </div>
+      <div class="tab-pane fade" id="lectures" role="tabpanel" aria-labelledby="lectures-tab">...</div>
+    </div>
+
 
 </div>
 
