7203f5f9bc8520ecd7610f54c8f371820d7b90c0
Thomas <thomasvsachs@gmail.com>
2020-06-20 21:44:07 +0200

    user datenbank default werte gesetzt für mylastcourse und instructor

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('instructor');
-            $table->string('my_last_course');
+            $table->boolean('instructor')->default(0);
+            $table->string('my_last_course')->default("");
             $table->rememberToken();
             $table->timestamps();
         });
