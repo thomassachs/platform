acf24e9749ec782f71908981e3bccba63d1c5e05
Thomas <thomasvsachs@gmail.com>
2020-06-28 19:48:49 +0200

    edit description angepasst

--- resources/views/instructor/editDescription.blade.php
+++ resources/views/instructor/editDescription.blade.php
@@ -23,28 +23,6 @@
       </li>
     </ul>
 
-    {{-- general tab --}}
-    <div class="tab-content" id="myTabContent">
-      <div class="tab-pane fade " id="general" role="tabpanel" aria-labelledby="general-tab">
-
-          {!! Form::open(['action' => ['CoursesController@update', $course->id], 'method' => 'POST']) !!}
-              <div class="form-group">
-                  {{ Form::label('title', 'Title') }}
-                  {{ Form::text('title', $course->title, ['class' => 'form-control', 'placeholder' => 'Title']) }}
-              </div>
-              <div class="form-group">
-                  {{ Form::label('language', 'Language') }}
-                  {{ Form::text('language', $course->language, ['class' => 'form-control', 'placeholder' => 'Language']) }}
-              </div>
-              <div class="form-group">
-                  {{ Form::label('game', 'Name of the Game') }}
-                  {{ Form::text('game', $course->game, ['class' => 'form-control', 'placeholder' => 'Gamename']) }}
-              </div>
-              {{ Form::hidden('_method', 'PUT') }}
-              {{ Form::submit('Edit', ['class' => 'btn btn-primary']) }}
-          {!! Form::close() !!}
-      </div>
-
 
       {{-- description tab --}}
       <div class="tab-pane fade show active" id="description" role="tabpanel" aria-labelledby="description-tab">
