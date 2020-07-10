c7d1a61fd4bc519cbf3781f7103930df86ade218
Thomas <thomasvsachs@gmail.com>
2020-07-10 22:40:07 +0200

    bilder versuche wieder rausgenommen

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,11 +4,5 @@
 
 this is the front pageeetttt
 <i class="fas fa-star"></i>
-{{-- {{Storage::url("/app/public/courses/inprogress/testkurss/11480c5bde9fd5786ef6d297c79a40ed_1593539535.jpg")}} --}}
-
-<img src="{{Storage::url("/courses/inprogress/testkurss/11480c5bde9fd5786ef6d297c79a40ed_1594412328.jpg")}}" width="250" height="150" alt="no image found">
-<img src="{{asset(config('app.file_path').'courses/inprogress/testkurss/11480c5bde9fd5786ef6d297c79a40ed_1594412328.jpg')}}" />
-
-<div style="background-image:url({{asset(config('app.file_path').'courses/inprogress/testkurss/11480c5bde9fd5786ef6d297c79a40ed_1594412328.jpg')}});">
 
 @endsection('content')
