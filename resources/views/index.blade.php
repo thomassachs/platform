8ccfea3b2afe6fcd770f1cdb227532f97d0418a6
Thomas <thomasvsachs@gmail.com>
2020-07-10 22:17:35 +0200

    test bild

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,5 +4,8 @@
 
 this is the front pageeetttt
 <i class="fas fa-star"></i>
+{{-- {{Storage::url("/app/public/courses/inprogress/testkurss/11480c5bde9fd5786ef6d297c79a40ed_1593539535.jpg")}} --}}
+
+<img src="{{Storage::url("/courses/inprogress/testkurss/11480c5bde9fd5786ef6d297c79a40ed_1594411698.jpg")}}" width="250" height="150" alt="no image found">
 
 @endsection('content')
