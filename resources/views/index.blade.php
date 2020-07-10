b7b60d9ccb26c48152689fbe765fd19ebe5dd6d1
Thomas <thomasvsachs@gmail.com>
2020-07-10 22:26:42 +0200

    test bild

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,5 +7,5 @@ this is the front pageeetttt
 {{-- {{Storage::url("/app/public/courses/inprogress/testkurss/11480c5bde9fd5786ef6d297c79a40ed_1593539535.jpg")}} --}}
 
 <img src="{{Storage::url("/courses/inprogress/testkurss/11480c5bde9fd5786ef6d297c79a40ed_1594412328.jpg")}}" width="250" height="150" alt="no image found">
-
+<img src="{{ asset('public/courses/inprogress/testkurss/11480c5bde9fd5786ef6d297c79a40ed_1594412328.jpg') }}" />
 @endsection('content')
