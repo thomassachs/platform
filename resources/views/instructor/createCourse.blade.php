86f55a4e05f0b992a5495746360990afe250eee7
Thomas <thomasvsachs@gmail.com>
2020-08-22 11:17:41 +0200

    create course blade fertiggestellt

--- resources/views/instructor/createCourse.blade.php
+++ resources/views/instructor/createCourse.blade.php
@@ -1,46 +1,98 @@
 @extends('layouts.app')
 
+@section('stylesheets')
+    <link href="{{ asset('css/instructor-dashboard.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/instructor-responsive.css') }}" rel="stylesheet">
+@endsection
+
+@section('scripts')
+    <script src="{{ asset('js/custom1.js') }}"></script>
+@endsection
+
 @section('instructorContent')
 
-@include('instructor.sidebar')
+    <div class="wrapper">
+		<div class="sa4d25">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<h2 class="st_title"><i class="uil uil-analysis"></i> Create New Course</h2>
+					</div>
+				</div>
+                @include('inc.messages')
+				<div class="row">
+					<div class="col-12">
+						<div class="">
+							<div id="add-course-tab" class="step-app">
+								<div class="step-content">
+									<div class="step-tab-panel step-tab-info active" id="tab_step1">
+										<div class="tab-from-content">
+											<div class="course__form">
+												<div class="general_info10">
 
-<div class="col-md-8">
+                                                    {!! Form::open(['action' => 'CoursesController@store', 'method' => 'POST']) !!}
+    													<div class="row">
+    														<div class="col-lg-6 col-md-6">
+    															<div class="ui search focus mt-30 lbel25">
+    																<label>Course Title*</label>
+    																<div class="ui left icon input swdh19">
+                                                                        {{ Form::text('title', '', ['class' => 'prompt srch_explore', 'placeholder' => 'Insert your course title.']) }}
+    																	<div class="badge_num">60</div>
+    																</div>
+    															</div>
+    														</div>
+    														<div class="col-lg-6 col-md-6">
+    															<div class="ui search focus mt-30 lbel25">
+    																<label>Course Subtitle* - momentan ein unnützes feld</label>
+    																<div class="ui left icon input swdh19">
+    																	<input class="prompt srch_explore" type="text" placeholder="Insert your course Subtitle." name="subtitle" data-purpose="edit-course-title" maxlength="60" id="sub[title]" value="">
+    																	<div class="badge_num2">120</div>
+    																</div>
+    															</div>
+    														</div>
+    														<div class="col-lg-4 col-md-12">
+    															<div class="mt-30 lbel25">
+    																<label>Language*</label>
+    															</div>
+                                                                {!!  Form::select('language',['english' => 'English','german' => 'German'],null,
+                                                                    ['class'=>'ui hj145 dropdown cntry152 prompt srch_explore','placeholder'=>'Select Language']) !!}
+    														</div>
+    														<div class="col-lg-4 col-md-6">
+    															<div class="mt-30 lbel25">
+    																<label>Name of the Game*</label>
+    															</div>
+                                                                {!!  Form::select('game',['leagueoflegends' => 'League of Legends','legendsofruneterra' => 'Legends of Runeterra'],null,
+                                                                    ['class'=>'ui hj145 dropdown cntry152 prompt srch_explore','placeholder'=>'Select Language']) !!}
+    														</div>
+    														<div class="col-lg-4 col-md-6">
+    															<div class="mt-30 lbel25">
+    																<label>Course Subcategory* - momentan ein unnützes feld</label>
+    															</div>
+    															<select class="ui hj145 dropdown cntry152 prompt srch_explore">
+    																<option value="">Select Subcategory</option>
+    																<option value="1">Javascript</option>
+    																<option value="2">Angular</option>
+    															</select>
+    														</div>
+    													</div>
+                                                        <div class="step-footer step-tab-pager">
+                                                            {{ Form::submit('Create Course', ['class' => 'btn btn-default steps_btn']) }}
+                        								</div>
+                                                    {!! Form::close() !!}
 
-    {{-- this is for the alerts when somebody passes the form incorrectly --}}
-    @include('inc.messages')
-
-    <h1>Create a new Course</h1>
-
-    @if ($coursesInProgress < 5)
-        {!! Form::open(['action' => 'CoursesController@store', 'method' => 'POST', 'enctype' => 'multipart&data']) !!}
-            <div class="form-group">
-                {{ Form::label('title', 'Title*') }}
-                {{ Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title']) }}
-            </div>
-            <div class="form-group">
-                {{ Form::label('language', 'Language*') }}
-                {{ Form::text('language', '', ['class' => 'form-control', 'placeholder' => 'Language']) }}
-            </div>
-            <div class="form-group">
-                {{ Form::label('game', 'Name of the Game*') }}
-                {{ Form::text('game', '', ['class' => 'form-control', 'placeholder' => 'Gamename']) }}
-            </div>
-            {{ Form::submit('Create', ['class' => 'btn btn-primary']) }}
-        {!! Form::close() !!}
-    @else
-        <p>We are sorry to tell you, but you can only have 5 courses with the status "inprogress" at the same time. Before You can create another course you have to delete one
-        or finish a course</p>
-    @endif
+												</div>
+											</div>
+										</div>
+									</div>
 
 
-
-
-
-
-
-</div>
-
-    <!-- end row from sidebar -->
-    </div>
+								</div>
+							</div>
+                        </div>
+                    </div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 @endsection('instructorContent')
---
+++ resources/views/oldViews/createCourse.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('instructorContent')
+
+@include('instructor.sidebar')
+
+<div class="col-md-8">
+
+    {{-- this is for the alerts when somebody passes the form incorrectly --}}
+    @include('inc.messages')
+
+    <h1>Create a new Course</h1>
+
+    @if ($coursesInProgress < 5)
+        {!! Form::open(['action' => 'CoursesController@store', 'method' => 'POST', 'enctype' => 'multipart&data']) !!}
+            <div class="form-group">
+                {{ Form::label('title', 'Title*') }}
+                {{ Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title']) }}
+            </div>
+            <div class="form-group">
+                {{ Form::label('language', 'Language*') }}
+                {{ Form::text('language', '', ['class' => 'form-control', 'placeholder' => 'Language']) }}
+            </div>
+            <div class="form-group">
+                {{ Form::label('game', 'Name of the Game*') }}
+                {{ Form::text('game', '', ['class' => 'form-control', 'placeholder' => 'Gamename']) }}
+            </div>
+            {{ Form::submit('Create', ['class' => 'btn btn-primary']) }}
+        {!! Form::close() !!}
+    @else
+        <p>We are sorry to tell you, but you can only have 5 courses with the status "inprogress" at the same time. Before You can create another course you have to delete one
+        or finish a course</p>
+    @endif
+
+
+
+
+
+
+
+</div>
+
+    <!-- end row from sidebar -->
+    </div>
+
+@endsection('instructorContent')
